6ada4ac9c609c93a065706a09290627c234de610
r0n92uya0 <rzy1145141919810rzy@gmail.com>
2026-02-13 23:27:27 +0800

    test: add test case for compile i32 type handling

--- tests/test_compiler.rs
+++ tests/test_compiler.rs
@@ -1,5 +1,9 @@
 use is::compiler;
 
 #[test]
-fn test_compiler() {
+fn test_i32_handling() {
+  let src = "let a: i32 = 0;";
+  let exp = "int a = 0;";
+  let res = compiler(src);
+  assert_eq!(res, exp)
 }
