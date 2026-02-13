bc2b996fcd28aa3e6a65c9d20693b9e3433d43c5
r0n92uya0 <rzy1145141919810rzy@gmail.com>
2026-02-13 23:58:24 +0800

    refactor(test): use r-string for test source

--- tests/test_compiler.rs
+++ tests/test_compiler.rs
@@ -2,7 +2,7 @@ use is::compiler;
 
 #[test]
 fn test_i32_handling() {
-  let src = "let a: i32 = 0;";
+  let src = r#"let a: i32 = 0;"#;
   let exp = "int a = 0;";
   let res = compiler(src);
   assert_eq!(res, exp)
